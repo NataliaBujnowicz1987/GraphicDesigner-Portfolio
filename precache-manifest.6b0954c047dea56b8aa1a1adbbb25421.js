4cf4d2667eacc4ec8e2f3626f338fcb15a4767ee
Natalia Bujnowicz <nataliabujnowicz1987@gmail.com>
2020-06-08 10:27:10 +0100

    Updates

---
+++ precache-manifest.6b0954c047dea56b8aa1a1adbbb25421.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "815f82b7ac471681a177443e41c2cf09",
+    "url": "/GraphicDesigner-Portfolio/static/media/portfolio-02.815f82b7.jpg"
+  },
+  {
+    "revision": "32e75ef759ccd6ffb32a",
+    "url": "/GraphicDesigner-Portfolio/static/css/main.c261cf76.chunk.css"
+  },
+  {
+    "revision": "150c7c2b52c72c875d17",
+    "url": "/GraphicDesigner-Portfolio/static/js/runtime~main.63323bb6.js"
+  },
+  {
+    "revision": "f0572d70e40f7ca06ca6",
+    "url": "/GraphicDesigner-Portfolio/static/js/2.e0dc5141.chunk.js"
+  },
+  {
+    "revision": "9d1816eef39402a069dc56007a6b80ca",
+    "url": "/GraphicDesigner-Portfolio/static/media/logo.9d1816ee.png"
+  },
+  {
+    "revision": "6e38dfcc2b29a6cdc91bfbfb5201f6fd",
+    "url": "/GraphicDesigner-Portfolio/static/media/img-ian.6e38dfcc.jpg"
+  },
+  {
+    "revision": "36d98574c4b9e207d109422be252e76f",
+    "url": "/GraphicDesigner-Portfolio/static/media/img.36d98574.jpg"
+  },
+  {
+    "revision": "8800e996ec552d29e0868b664216af81",
+    "url": "/GraphicDesigner-Portfolio/static/media/portfolio-01.8800e996.jpg"
+  },
+  {
+    "revision": "32e75ef759ccd6ffb32a",
+    "url": "/GraphicDesigner-Portfolio/static/js/main.aa994c66.chunk.js"
+  },
+  {
+    "revision": "501ffb32a233773653959c18b3b29fd1",
+    "url": "/GraphicDesigner-Portfolio/static/media/portfolio-03.501ffb32.jpg"
+  },
+  {
+    "revision": "56bebba0a2af689297eb7101a58dfc02",
+    "url": "/GraphicDesigner-Portfolio/static/media/portfolio-04.56bebba0.jpg"
+  },
+  {
+    "revision": "4678d01a6c5747cfc12f760fbe109473",
+    "url": "/GraphicDesigner-Portfolio/static/media/portfolio-05.4678d01a.jpg"
+  },
+  {
+    "revision": "d27e6c31ba956a850afbcfd561a1a9bd",
+    "url": "/GraphicDesigner-Portfolio/static/media/portfolio-06.d27e6c31.jpg"
+  },
+  {
+    "revision": "0996728ab336520cca80d024dadbc490",
+    "url": "/GraphicDesigner-Portfolio/static/media/portfolio-07.0996728a.jpg"
+  },
+  {
+    "revision": "cd574afdb8ffa98f5a2929bb2c9260ae",
+    "url": "/GraphicDesigner-Portfolio/static/media/portfolio-08.cd574afd.jpg"
+  },
+  {
+    "revision": "a99eac84e53d4dfa459cf8df03f9a40d",
+    "url": "/GraphicDesigner-Portfolio/static/media/services-bg.a99eac84.jpg"
+  },
+  {
+    "revision": "ddea9ed3517449b2bcc6463c970c0518",
+    "url": "/GraphicDesigner-Portfolio/index.html"
+  }
+];
